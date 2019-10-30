13290a8be7e91b29e4a43aa71ba58553642021b4
Eric Marchand <eric.marchand@4d.com>
2019-10-30 10:31:13 +0100

    Mock more functions. Add logger

--- Package.swift
+++ Package.swift
@@ -14,13 +14,12 @@ let package = Package(
             targets: ["QMobileUI"]),
     ],
     dependencies: [
-        // Dependencies declare other packages that this package depends on.
-        // .package(url: /* package url */, from: "1.0.0"),
+        .package(url: "https://github.com/DaveWoodCom/XCGLogger.git", from: "7.0.0"),
     ],
     targets: [
         .target(
             name: "QMobileUI",
-            dependencies: [],
+            dependencies: ["XCGLogger"],
             path: "Sources")
     ]
 )
--- Sources/Mock.swift
+++ Sources/Mock.swift
@@ -6,16 +6,93 @@
 import Foundation
 import UIKit
 
-open class ListForm___LISTFORMTYPE___: UIViewController {
+public protocol ListForm {
+    var tableName: String { get }
+    var dataSource: DataSource? { get }
+    func onLoad()
+    func onWillAppear(_ animated: Bool)
+    func onDidAppear(_ animated: Bool)
+    func onWillDisappear(_ animated: Bool)
+    func onDidDisappear(_ animated: Bool)
+}
+
+public protocol ListFormSearchable: ListForm, UISearchBarDelegate {
+    var searchBar: UISearchBar! { get set }
+    var searchableAsTitle: Bool { get }
+    var searchableWhenScrolling: Bool { get }
+    var searchableHideNavigation: Bool { get }
+
+    func onSearchBegin()
+    func onSearchButtonClicked()
+    func onSearchFetching()
+    func onSearchCancel()
+}
+
+open class ListForm___LISTFORMTYPE___: UIViewController, ListFormSearchable {
     open var tableName: String { return "" }
+    open var dataSource: DataSource? { return nil }
     open func onLoad() {}
     open func onWillAppear(_ animated: Bool) {}
     open func onDidAppear(_ animated: Bool) {}
     open func onWillDisappear(_ animated: Bool) {}
     open func onDidDisappear(_ animated: Bool) {}
+
+    open var searchBar: UISearchBar!
+    open var searchableAsTitle: Bool = true
+    open var searchableWhenScrolling: Bool = true
+    open var searchableHideNavigation: Bool = true
+
+    open func onSearchBegin() {}
+    open func onSearchButtonClicked() {}
+    open func onSearchFetching() {}
+    open func onSearchCancel() {}
 }
-open class ListFormTable: ListForm___LISTFORMTYPE___ {}
-open class ListFormCollection: ListForm___LISTFORMTYPE___ {}
+open class ListFormTable: UITableViewController, ListFormSearchable {
+    open var tableName: String { return "" }
+    open var dataSource: DataSource? { return nil }
+    open func onLoad() {}
+    open func onWillAppear(_ animated: Bool) {}
+    open func onDidAppear(_ animated: Bool) {}
+    open func onWillDisappear(_ animated: Bool) {}
+    open func onDidDisappear(_ animated: Bool) {}
+
+    open var searchBar: UISearchBar!
+    open var searchableAsTitle: Bool = true
+    open var searchableWhenScrolling: Bool = true
+    open var searchableHideNavigation: Bool = true
+
+    open func onSearchBegin() {}
+    open func onSearchButtonClicked() {}
+    open func onSearchFetching() {}
+    open func onSearchCancel() {}
+}
+open class ListFormCollection: UICollectionViewController, ListFormSearchable {
+    open var tableName: String { return "" }
+    open var dataSource: DataSource? { return nil }
+    open func onLoad() {}
+    open func onWillAppear(_ animated: Bool) {}
+    open func onDidAppear(_ animated: Bool) {}
+    open func onWillDisappear(_ animated: Bool) {}
+    open func onDidDisappear(_ animated: Bool) {}
+
+    open var searchBar: UISearchBar!
+    open var searchableAsTitle: Bool = true
+    open var searchableWhenScrolling: Bool = true
+    open var searchableHideNavigation: Bool = true
+
+    open func onSearchBegin() {}
+    open func onSearchButtonClicked() {}
+    open func onSearchFetching() {}
+    open func onSearchCancel() {}
+}
+
+// MARK: - data
+
+public protocol DetailsForm {}
+extension DetailsForm {
+    public var record: Record? { return nil }
+}
+
 open class DetailsForm___DETAILFORMTYPE___: UIViewController, DetailsForm {
     open func onLoad() {}
     open func onWillAppear(_ animated: Bool) {}
@@ -25,9 +102,10 @@ open class DetailsForm___DETAILFORMTYPE___: UIViewController, DetailsForm {
 }
 open class DetailsFormBare: DetailsForm___DETAILFORMTYPE___ {}
 
-public protocol DetailsForm {}
-extension DetailsForm {
-    public var record: Record? { return nil }
+
+// MARK: - data
+public protocol DataSource {
+    func performFetch()
 }
 open class ___TABLE___ {
 
@@ -44,6 +122,8 @@ public class Record: NSObject {
 
 }
 
+// MARK: - view binding
+
 public extension UIView {
 
     var bindTo: BindTo {
@@ -57,3 +137,7 @@ public class BindTo {
         self.record = record
     }
 }
+
+// MARK: - Logger
+import XCGLogger
+public let logger = XCGLogger.default
