2c818c0e5c74219de13a91df886e7aaa7ae5d6b4
Eric Marchand <phimage@users.noreply.github.com>
2019-11-06 12:02:42 +0100

    Add IBAnimatable

--- Package.swift
+++ Package.swift
@@ -14,12 +14,13 @@ let package = Package(
             targets: ["QMobileUI"]),
     ],
     dependencies: [
+        .package(url: "https://github.com/IBAnimatable/IBAnimatable.git", .upToNextMajor(from: "6.0.0")),
         .package(url: "https://github.com/DaveWoodCom/XCGLogger.git", from: "7.0.0"),
     ],
     targets: [
         .target(
             name: "QMobileUI",
-            dependencies: ["XCGLogger"],
+            dependencies: ["XCGLogger", "IBAnimatable"],
             path: "Sources")
     ]
 )
