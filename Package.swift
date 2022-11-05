7c558af23675250c13ebb59b8cfc5ae5dbc22791
Eric Marchand <59135882+mesopelagique@users.noreply.github.com>
2022-11-05 10:58:34 +0100

    Update Package.swift
    to add Eureka

--- Package.swift
+++ Package.swift
@@ -20,13 +20,14 @@ let package = Package(
         //.package(url: "https://github.com/devicekit/DeviceKit.git", .upToNextMajor(from: "2.3.0")),
         .package(url: "https://github.com/phimage/DeviceKit.git", .branch("feature/macos")),
         .package(url: "https://github.com/DaveWoodCom/XCGLogger.git", from: "7.0.0"),
+        .package(url: "https://github.com/xmartlabs/Eureka.git", .revision("HEAD")),
         .package(url: "https://github.com/4d-for-ios/QMobileAPI.git", .revision("HEAD")),
         .package(url: "https://github.com/4d-for-ios/QMobileDataSync.git", .revision("HEAD"))
     ],
     targets: [
         .target(
             name: "QMobileUI",
-            dependencies: ["XCGLogger", "IBAnimatable", "DeviceKit", "Prephirences", "QMobileAPI", "QMobileDataSync"],
+            dependencies: ["XCGLogger", "IBAnimatable", "DeviceKit", "Prephirences", "Eureka", "QMobileAPI", "QMobileDataSync"],
             path: "Sources")
     ]
 )
